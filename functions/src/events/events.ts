f8549a5c926b86aef24a7dba86e48ae859d27467
Eralp Karaduman <eralp@eralpkaraduman.com>
2019-07-02 19:05:25 +0300

    Added event categories

--- functions/src/events/events.ts
+++ functions/src/events/events.ts
@@ -1,27 +1,73 @@
-const words = [
-  'pizza', 'pizzas',
-  'snack', 'snacks',
-  'beer', 'beers',
-  'meal', 'food'
-]
-const occurances = [
-  '🍔', '🍕', '🌭', '🌮'
-]
-const regExpWordList = words.reduce((list, word, index) =>
-  `${list}${index ? '|' : ''}${word}`, '(') + ')'
-const dictionaryCheckRegExpString = `\\b${regExpWordList}\\b`
-const dictionaryCheckRegExp = new RegExp(dictionaryCheckRegExpString)
-export const containsWordInDictionary = (textString: string) => {
-  return dictionaryCheckRegExp.test((textString || '').toLocaleLowerCase()) ||
-  occurances.some(occ => textString.includes(occ));
+import _ from "lodash";
+const foods = [
+  "pizza",
+  "pizzas",
+  "snack",
+  "snacks",
+  "taco",
+  "tacos",
+  "hotdog",
+  "hotdogs",
+  "hot dog",
+  "hot dogs",
+  "meal",
+  "food",
+  "taste",
+  "tasting"
+];
+const foodsOccurances = ["🍔", "🍕", "🌭", "🌮"];
+const drinks = ["beer", "beers", "drink", "drinks", "wine", "wines"];
+const drinksOccurances = ["🍺", "🍻", "🍷", "🍾", "🍸", "☕️"];
+
+export enum CATEGORY {
+  FOOD = "food",
+  DRINK = "drink"
 }
 
+const categories: Array<{
+  id: CATEGORY;
+  words: Array<string>;
+  occurances: Array<string>;
+}> = [
+  { id: CATEGORY.FOOD, words: foods, occurances: foodsOccurances },
+  { id: CATEGORY.DRINK, words: drinks, occurances: drinksOccurances }
+];
+
+const createMatcher = (words: Array<string>, occurances: Array<string>) => {
+  const regExpWordList =
+    words.reduce(
+      (list, word, index) => `${list}${index ? "|" : ""}${word}`,
+      "("
+    ) + ")";
+  const dictionaryCheckRegExpString = `\\b${regExpWordList}\\b`;
+  const dictionaryCheckRegExp = new RegExp(dictionaryCheckRegExpString);
+  return (textString: string) =>
+    dictionaryCheckRegExp.test((textString || "").toLocaleLowerCase()) ||
+    occurances.some(occ => textString.includes(occ));
+};
+
+const matchers = categories.map(({ id, words, occurances }) => ({
+  id,
+  matcher: createMatcher(words, occurances)
+}));
+
+export const containsWordInDictionary = (
+  textString: string
+): null | { [id: string]: boolean } => {
+  const matches = matchers.reduce(
+    (matches, { id, matcher }) =>
+      matcher(textString) ? { ...matches, [id]: true } : matches,
+    {}
+  );
+  return _.isEmpty(matches) ? false : matches;
+};
+
 export interface POEvent {
-  name: string
-  time: string,
-  lat: string,
-  lon: string,
-  venueName: string,
-  link: string
-  description: string
+  name: string;
+  time: string;
+  lat: string;
+  lon: string;
+  venueName: string;
+  link: string;
+  description: string;
 }
--- functions/src/providers/meetup/meetup.ts
+++ functions/src/providers/meetup/meetup.ts
@@ -1,54 +1,64 @@
-import fetch from 'node-fetch'
-import keys from '../../keys.json'
-import {POEvent} from '../../events/events'
-import jsdom from 'jsdom'
+import fetch from "node-fetch";
+import keys from "../../keys.json";
+import { POEvent } from "../../events/events";
+import jsdom from "jsdom";
 const { JSDOM } = jsdom;
 
-export const createAuthorizationUrl = (state: string) => 'https://secure.meetup.com/oauth2/authorize' +
-`?client_id=${keys.meetup.oauth.key}` +
-`&response_type=code` +
-`&state=${state}` +
-`&redirect_uri=${keys.meetup.oauth.redirectUri}`;
+export const createAuthorizationUrl = (state: string) =>
+  "https://secure.meetup.com/oauth2/authorize" +
+  `?client_id=${keys.meetup.oauth.key}` +
+  `&response_type=code` +
+  `&state=${state}` +
+  `&redirect_uri=${keys.meetup.oauth.redirectUri}`;
 
 interface AccessTokenResponse {
-  access_token: string,
-  expires_in: string,
-  refresh_token: string
-};
+  access_token: string;
+  expires_in: string;
+  refresh_token: string;
+}
 
-export const getAccessToken = async (code: string): Promise<AccessTokenResponse> => {
-  const response = await fetch('https://secure.meetup.com/oauth2/access', {
-    method: 'POST',
+export const getAccessToken = async (
+  code: string
+): Promise<AccessTokenResponse> => {
+  const response = await fetch("https://secure.meetup.com/oauth2/access", {
+    method: "POST",
     body:
-      `client_id=${keys.meetup.oauth.key}`+
-      `&client_secret=${keys.meetup.oauth.secret}`+
-      '&grant_type=authorization_code'+
-      `&redirect_uri=${keys.meetup.oauth.redirectUri}`+
+      `client_id=${keys.meetup.oauth.key}` +
+      `&client_secret=${keys.meetup.oauth.secret}` +
+      "&grant_type=authorization_code" +
+      `&redirect_uri=${keys.meetup.oauth.redirectUri}` +
       `&code=${code}`,
     headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
+      "Content-Type": "application/x-www-form-urlencoded"
     }
-  })
-  const responseJson = await response.json()
-  return responseJson as AccessTokenResponse
-}
+  });
+  const responseJson = await response.json();
+  return responseJson as AccessTokenResponse;
+};
 
-export const getEvents = async (accessToken: string, lat: string, lon: string): Promise<POEvent[]> => {
-  const url = 'https://api.meetup.com/find/upcoming_events' + 
+export const getEvents = async (
+  accessToken: string,
+  lat: string,
+  lon: string
+): Promise<POEvent[]> => {
+  const url =
+    "https://api.meetup.com/find/upcoming_events" +
     `?lat=${lat}` +
-    `&lon=${lon}`
+    `&lon=${lon}`;
   const response = await fetch(url, {
     headers: { Authorization: `Bearer ${accessToken}` }
-  })
-  const responseJson = await response.json()
+  });
+  const responseJson = await response.json();
   // TODO: const city = responseJson.city  // { city: 'Helsinki', country: 'fi', ... }
-  return responseJson.events.map((event: any): POEvent => ({
-    name: event.name,
-    time: event.time,
-    lat: event.venue && event.venue.lat,
-    lon: event.venue && event.venue.lon,
-    venueName: event.venue && event.venue.name,
-    link: event.link,
-    description: JSDOM.fragment(event.description || '').textContent as string 
-  }))
-}
+  return responseJson.events.map(
+    (event: any): POEvent => ({
+      name: event.name,
+      time: event.time,
+      lat: event.venue && event.venue.lat,
+      lon: event.venue && event.venue.lon,
+      venueName: event.venue && event.venue.name,
+      link: event.link,
+      description: JSDOM.fragment(event.description || "").textContent as string
+    })
+  );
+};
